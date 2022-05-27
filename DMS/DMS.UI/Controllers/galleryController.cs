c1927a5de8ce9684aafeafc78e0f339c8132c4f4
kushal <kushalpathak18@gmail.com>
2022-05-27 09:21:55 +0545

    button

---
+++ DMS/DMS.UI/Controllers/galleryController.cs
@@ -0,0 +1,20 @@
+﻿using DMS.DAL.newDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    public class galleryController : Controller
+    {
+        newsEntities db = new newsEntities();
+        // GET: gallery
+        public ActionResult newsPage()
+        {
+            List<recentNew> all_data = db.recentNews.ToList();
+            return View(all_data);
+        }
+    }
+}
---
+++ DMS/DMS.UI/Controllers/recentNewsController.cs
@@ -0,0 +1,21 @@
+﻿using DMS.DAL.newDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    public class recentNewsController : Controller
+    {
+        newsEntities db = new newsEntities();
+        // GET: recentNews
+        public ActionResult recentNewPage()
+        {
+            List<recentNew> all_data = db.recentNews.ToList();
+            return View(all_data);
+        }
+
+    }
+}
